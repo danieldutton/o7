5a69447fdf66ec3a9bb2286c566cd93d566cb29c
Daniel Dutton <DannyDuttster@gmail.com>
2021-06-04 21:27:04 +0100

    Initial Commit

---
+++ o7/o7App.swift
@@ -0,0 +1,17 @@
+//
+//  o7App.swift
+//  o7
+//
+//  Created by Daniel Dutton on 04/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct o7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
