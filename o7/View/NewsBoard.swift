9ed87831b1e0aaa6404da6523fae5fd0995f5056
Daniel Dutton <DannyDuttster@gmail.com>
2021-06-13 14:53:31 +0100

    Add ProgressView.  Hides when loading complete

--- o7/View/NewsBoard.swift
+++ o7/View/NewsBoard.swift
@@ -3,43 +3,53 @@ import SwiftUI
 struct NewsBoard: View {
     @StateObject var viewModel = NewsBoardViewModel()
     @State private var displayInfoAlert: Bool = false
-    
+
     var body: some View {
         NavigationView {
-            List(viewModel.newsItems) { newsItem in
-                NavigationLink(destination: NewsItemDetail(newsItem: newsItem)) {
-                    LazyVStack(alignment: .leading, spacing: 5) {
-                        Label(newsItem.date, systemImage: "calendar")
-                            .font(.callout)
-                        Text(newsItem.title)
-                            .font(.headline)
-                            .lineLimit(0)
-                            .truncationMode(.tail)
-                        Text(newsItem.body)
-                            .font(.body)
-                            .lineLimit(5)
+            ZStack {
+                List(viewModel.newsItems) { newsItem in
+                    NavigationLink(destination: NewsItemDetail(newsItem: newsItem)) {
+                        LazyVStack(alignment: .leading, spacing: 5) {
+                            Label(newsItem.date, systemImage: "calendar")
+                                .font(.callout)
+                            Text(newsItem.title)
+                                .font(.headline)
+                                .lineLimit(0)
+                                .truncationMode(.tail)
+                            Text(newsItem.body)
+                                .font(.body)
+                                .lineLimit(5)
+                        }
                     }
                 }
+                .listStyle(PlainListStyle())
+                .navigationBarTitle("Galnet News")
+                .navigationBarTitleDisplayMode(.large)
+                .navigationBarItems(leading: Button(action: {
+                    displayInfoAlert.toggle()
+                }, label: {
+                    Image(systemName: "info.circle")
+                }), trailing: Button(action: {
+                    //refresh list
+                }, label: {
+                    Image(systemName: "arrow.clockwise")
+                }))
+                
+                ProgressView()
+                    .scaleEffect(1.4)
+                    .opacity(viewModel.isPerformingWork ? 0: 1)
             }
-            .listStyle(PlainListStyle())
-            .navigationBarTitle("Galnet News")
-            .navigationBarTitleDisplayMode(.large)
-            .navigationBarItems(leading: Button(action: {
-                displayInfoAlert.toggle()
-            }, label: {
-                Image(systemName: "info.circle")
-            }), trailing: Button(action: {
-                //refresh list
-            }, label: {
-                Image(systemName: "arrow.clockwise")
-            } ))
         }
         .alert(isPresented: $displayInfoAlert, content: {
-            Alert(
-                title: Text("Acknowledgements"),
-                message: Text("Elite Dangerous, c 1984 - 2019 Frontier Developments Plc.  All rights reserved."),
-                dismissButton: .cancel(Text("OK")))
+            copyrightAlert()
         })
+    }
+    
+    private func copyrightAlert() -> Alert {
+        Alert(
+            title: Text("Acknowledgements"),
+            message: Text("Elite Dangerous, c 1984 - 2019 Frontier Developments Plc.  All rights reserved."),
+            dismissButton: .cancel(Text("OK")))
     }
 }
 
@@ -47,8 +57,10 @@ struct NewsBoard_Previews: PreviewProvider {
     static var previews: some View {
         Group {
             NewsBoard()
+                .previewDevice(PreviewDevice(rawValue: "iPhone 12"))
                 .colorScheme(.light)
             NewsBoard()
+                .previewDevice(PreviewDevice(rawValue: "iPhone 12"))
                 .colorScheme(.dark)
         }
     }
--- o7/ViewModel/NewsBoardViewModel.swift
+++ o7/ViewModel/NewsBoardViewModel.swift
@@ -7,6 +7,7 @@ class NewsBoardViewModel: ObservableObject {
     private var newsService: NewsService
     private var newsSanitiser: NewsSanitiser
     @Published var newsItems: [NewsItem] = []
+    @Published var isPerformingWork = false
     
     init(newsService: NewsService = GalnetNewsWebService(),
          newsSanitiser: NewsSanitiser = HTMLNewsSanitiser()) {
@@ -18,10 +19,12 @@ class NewsBoardViewModel: ObservableObject {
     
     func loadNewsBoard() {
         Just(Bundle.main.url(forResource: "sample_news", withExtension: "json")!)
+            .handleEvents(receiveRequest: { _ in self.isPerformingWork = true })
             .tryMap { try Data(contentsOf: $0)}
             .decode(type: [NewsItem].self, decoder: JSONDecoder())
             .replaceError(with: [NewsItem(date: "", title: "Preview Error", body: "")])
             .map { self.newsSanitiser.sanitise($0)}
+            .handleEvents(receiveRequest: { _ in self.isPerformingWork = false })
             .assign(to: \.newsItems, on: self)
             .store(in: &subscriptions)
          
